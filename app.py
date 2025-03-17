87a12fcdaa4f9c4ee1ff6dc923d7970929d6d5ef
djdhdfxf <143930810+djdhdfxf@users.noreply.github.com>
2025-03-17 19:22:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,1 @@
-def save_photo(file):  
-    import time  
-    filename = f"photo_{int(time.time())}.jpg"  
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)  
-    file.save(file_path)  
-    return file_path
-    
-if photo_path:  
-    send_photo_to_telegrambot(chat_id, photo_path)  
+
